92b65c59892d21a1c2fb6986aeb66949b141687f
tumffa <tuomas.v.niemi@gmail.com>
2025-01-10 15:07:42 +0200

    ex 8.6

--- library-backend.js
+++ library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -119,6 +120,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -130,11 +140,21 @@ const resolvers = {
       const genreBooks = args.genre ? authorBooks.filter(book => book.genres.includes(args.genre)) : authorBooks
       return genreBooks
     },
-    allAuthors: () => 
-      authors.map(author => ({
+    allAuthors: () => authors.map(author => ({
         name: author.name,
+        born: author.born,
         bookCount: books.filter(book => book.author === author.name).length
       }))
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      if (!authors.find(author => author.name === args.author)); {
+        authors = authors.concat({ name: args.author, id: uuid() })
+      }
+      return book
+    }
   }
 }
 
